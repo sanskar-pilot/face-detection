f573b8935ac512ba409ae86bf53c835fdbe84ac8
Your Name <you@example.com>
2023-02-12 19:04:40 +0530

    fix

---
+++ client/pages/home.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { Button, Input, Paper, Typography } from '@mui/material';
+import { useRouter } from 'next/router'
+
+export default function SimpleContainer() {
+  const router = useRouter()
+
+  const onClick = () => {
+    router.push('/home')
+  }
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', display:'flex', alignItems:'center', justifyContent:'center' }} >
+          <Paper>
+          <Typography variant='h5'>Home</Typography>
+
+            <Input />
+            <Button>Login</Button>
+          </Paper>
+          </Box>
+      </Container>
+    </React.Fragment>
+  );
+}
---
+++ server/src/login.py
@@ -0,0 +1,127 @@
+import os
+from tkinter import *
+import sqlite3
+from pathlib import Path
+from flask import Flask, render_template, Response
+from camera import VideoCamera
+
+
+app = Flask(__name__)
+
+root = Tk()
+root.title("Positive Attitude Conversion")
+width = 700
+height = 480
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x = (screen_width/2) - (width/2)
+y = (screen_height/2) - (height/2)
+root.geometry("%dx%d+%d+%d" % (width, height, x, y))
+root.resizable(0, 0)
+
+#==============================VARIABLES======================================
+USERNAME = StringVar()
+PASSWORD = StringVar()
+ 
+#==============================FRAMES=========================================
+Top = Frame(root, bd=2,  relief=RIDGE)
+Top.pack(side=TOP, fill=X)
+Form = Frame(root, height=200)
+Form.pack(side=TOP, pady=20)
+ 
+#==============================LABELS=========================================
+lbl_title = Label(Top, text = "Positive Attitude Conversion", font=('arial', 26))
+lbl_title.pack(fill=X)
+lbl_username = Label(Form, text = "Username:", font=('arial', 14), bd=15)
+lbl_username.grid(row=0, sticky="e")
+lbl_password = Label(Form, text = "Password:", font=('arial', 14), bd=15)
+lbl_password.grid(row=1, sticky="e")
+lbl_text = Label(Form)
+lbl_text.grid(row=2, columnspan=2)
+ 
+#==============================ENTRY WIDGETS==================================
+username = Entry(Form, textvariable=USERNAME, font=(14))
+username.grid(row=0, column=1)
+password = Entry(Form, textvariable=PASSWORD, show="*", font=(14))
+password.grid(row=1, column=1)
+ 
+#==========================================================================
+#==============================METHODS========================================
+def Database():
+    global conn, cursor
+    conn = sqlite3.connect("pythontut.db")
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS `member` (mem_id INTEGER NOT NULL PRIMARY KEY  AUTOINCREMENT, username TEXT, password TEXT)")       
+    cursor.execute("SELECT * FROM `member` WHERE `username` = 'admin' AND `password` = 'admin'")
+    if cursor.fetchone() is None:
+        cursor.execute("INSERT INTO `member` (username, password) VALUES('admin', 'admin')")
+        conn.commit()
+#=================================================================================        
+def Login(event=None):
+    Database()
+    if USERNAME.get() == "" or PASSWORD.get() == "":
+        lbl_text.config(text="Please complete the required field!", fg="red")
+    else:
+        cursor.execute("SELECT * FROM `member` WHERE `username` = ? AND `password` = ?", (USERNAME.get(), PASSWORD.get()))
+        if cursor.fetchone() is not None:
+           os.system('python emotions.py --mode display')
+           #exec(open('emotions.py').read())
+           # HomeWindow()
+            #USERNAME.set("")
+           # PASSWORD.set("")
+           # lbl_text.config(text="")
+        else:
+            lbl_text.config(text="Invalid username or password", fg="red")
+            USERNAME.set("")
+            PASSWORD.set("")   
+    cursor.close()
+    conn.close()
+ 
+def HomeWindow():
+    global Home
+    root.withdraw()
+    Home = Toplevel()
+    Home.title("Positive attitude coversion")
+    width = 800
+    height = 600
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x = (screen_width/2) - (width/2)
+    y = (screen_height/2) - (height/2)
+    root.resizable(0, 0)
+    Home.geometry("%dx%d+%d+%d" % (width, height, x, y))
+    lbl_home = Label(Home, text="Successfully Login!", font=('times new roman', 20)).pack()
+    btn_back = Button(Home, text='Back', command=Back).pack(pady=20, fill=X)
+ 
+def Back():
+    Home.destroy()
+    root.deiconify()	
+
+#==============================BUTTON WIDGETS=================================
+btn_login = Button(Form, text="Login", width=45, command=Login)
+btn_login.grid(pady=25, row=3, columnspan=2)
+btn_login.bind('<Return>', Login)
+
+#==============================INITIALIATION==================================
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    # root.mainloop()
+    app.run('0.0.0.0')
